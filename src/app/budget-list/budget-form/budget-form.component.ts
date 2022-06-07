2cbd274ddecd3d2443d59d4cef6dc6a54ba5bdf2
Patrick Leclerc <patrick.leclerc14@gmail.com>
2022-06-06 22:48:40 -0400

    Icomes and Expenses tables

--- src/app/budget-list/budget-form/budget-form.component.ts
+++ src/app/budget-list/budget-form/budget-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { BudgetsService } from 'src/app/services/budgets.service';
 
 @Component({
   selector: 'app-budget-form',
@@ -8,10 +9,15 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class BudgetFormComponent implements OnInit {
 
-
   budgetForm: FormGroup;
+  incomesTotal: number = 0;
+  expensesTotal: number = 0;
+  incomesList = [{type: "Salaire", amount: 75000, frequency: this.bugetService.getFrequencyDescription(5), annual: this.bugetService.convertToAnnualIncomes(75000, 5)}, 
+  {type: "Location immeuble", amount: 975, frequency: this.bugetService.getFrequencyDescription(4), annual: this.bugetService.convertToAnnualIncomes(975, 4)}];
+  expensesList = [{type: "Hypothèque", amount: 700, frequency: this.bugetService.getFrequencyDescription(4), annual: this.bugetService.convertToAnnualIncomes(700, 4)}, 
+  {type: "Taxes municipales", amount: 2500, frequency: this.bugetService.getFrequencyDescription(5), annual: this.bugetService.convertToAnnualIncomes(2500, 5)}];
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder, private bugetService: BudgetsService) { }
 
   ngOnInit(): void {
     this.budgetForm = this.formBuilder.group({
@@ -19,8 +25,15 @@ export class BudgetFormComponent implements OnInit {
       createdBy: [null],
       description: [null],
       dateFrom: [null],
-      dateTo: [null]
-    }); 
+      dateTo: [null],
+      income: {
+        type: [null],
+        amount: [null],
+        frequency: [null] 
+      }    
+    });
+    this.incomesTotal = this.bugetService.getAnnualTotal(this.incomesList);
+    this.expensesTotal = this.bugetService.getAnnualTotal(this.expensesList);
   }
 
 }
--- src/app/services/budgets.service.ts
+++ src/app/services/budgets.service.ts
@@ -5,5 +5,64 @@ import { Injectable } from '@angular/core';
 })
 export class BudgetsService {
 
+  frequencyList = [
+    {id: 0, description: "Par jour"}, 
+    {id: 1, description: "Par semaine"}, 
+    {id: 2, description: "Aux deux semaines"}, 
+    {id: 3, description: "Bimensuel"}, 
+    {id: 4, description: "Par mois"}, 
+    {id: 5, description: "Annuel"}
+  ];
+
   constructor() { }
+
+  getFrequencyDescription(id: number) {
+    var frequencyObject: any;
+    this.frequencyList.forEach(element => {
+      if (element.id == id) {
+        frequencyObject = element;
+      }
+    });    
+    const description = frequencyObject.description;
+    return description;
+  }
+  
+  convertToAnnualIncomes(amount: number, frequencyID: number) {
+    var annualAmount: number = 0;
+    switch (frequencyID) {
+      case(0):{
+        annualAmount = amount * 365;
+        break;
+      }
+      case(1):{
+        annualAmount = amount * 52.14;
+        break;
+      }
+      case(2):{
+        annualAmount = amount * 26.07;
+        break;
+      }
+      case(3):{
+        annualAmount = amount * 24;
+        break;
+      }
+      case(4):{
+        annualAmount = amount * 12;
+        break;
+      }
+      case(5):{
+        annualAmount = amount;
+        break;
+      }
+    };
+    return annualAmount;
+  }
+
+  getAnnualTotal(incomesList) {
+    var total: number = 0;
+    incomesList.forEach(element => {
+      total += element.annual;
+    });
+    return total;
+  }
 }
