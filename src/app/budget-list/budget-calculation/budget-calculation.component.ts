8e9a50cf043af6972723c0344773854282f23b57
Patrick Leclerc <patrick.leclerc14@gmail.com>
2022-06-17 17:16:44 -0400

    Budget calculation part 1

--- src/app/budget-list/budget-calculation/budget-calculation.component.ts
+++ src/app/budget-list/budget-calculation/budget-calculation.component.ts
@@ -12,25 +12,44 @@ export class BudgetCalculationComponent implements OnInit {
 
   budgetForm: FormGroup;
   currentBudget: Budget;
+  payerParts = new Array();
 
-  constructor(private formBuilder: FormBuilder, private budgetsService: BudgetsService) { }
+
+  constructor(private formBuilder: FormBuilder, private budgetsService: BudgetsService) { 
+    this.currentBudget = this.budgetsService.currentBudget;
+   }
 
   ngOnInit(): void {
     this.initForm();
   }
 
   initForm() {
-    this.currentBudget = this.budgetsService.currentBudget;
-    this.budgetForm.setValue(this.currentBudget);
+    this.getCurrentBudget();
     this.budgetForm = this.formBuilder.group({
-      id: this.currentBudget.id,
-      title: this.currentBudget.title,
-      createdBy: this.currentBudget.createdBy,
-      description: this.currentBudget.description,
-      dateFrom: this.currentBudget.dateFrom,
-      dateTo: this.currentBudget.dateTo,
-      incomes: this.currentBudget.incomes,
-      expenses: this.currentBudget.expenses
+      id:  this.currentBudget.id,
+      title:  this.currentBudget.title,
+      createdBy:  this.currentBudget.createdBy,
+      description:  this.currentBudget.description,
+      dateFrom:  this.currentBudget.dateFrom,
+      dateTo:  this.currentBudget.dateTo,
+      incomes:  this.currentBudget.incomes,
+      expenses:  this.currentBudget.expenses
     });
   }
+
+  getCurrentBudget() {
+    this.currentBudget.id = this.budgetsService.currentBudget.id,
+    this.currentBudget.title = this.budgetsService.currentBudget.title,
+    this.currentBudget.createdBy = this.budgetsService.currentBudget.createdBy,
+    this.currentBudget.description = this.budgetsService.currentBudget.description,
+    this.currentBudget.dateFrom = this.budgetsService.currentBudget.dateFrom,
+    this.currentBudget.dateTo = this.budgetsService.currentBudget.dateTo,
+    this.currentBudget.incomes = this.budgetsService.currentBudget.incomes,
+    this.currentBudget.expenses = this.budgetsService.currentBudget.expenses
+  }
+
+  getPayerParts() {
+    console.log("Payer parts: ");
+  }
+
 }
